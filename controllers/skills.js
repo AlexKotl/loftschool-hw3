79efca1eb7e477d836f61df7ef585df0ef091d5b
Alex <slicer256@gmail.com>
2019-06-20 00:24:36 +0300

    saving skills data using lowdb module

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
 exports.get = () => new Promise((resolve, reject) => {
   try {
     resolve();
@@ -9,7 +14,14 @@ exports.get = () => new Promise((resolve, reject) => {
 
 exports.set = ({age, concerts, cities, years}) => new Promise((resolve, reject) => {
   try {
-    console.log("Saving", age, concerts, years);
+    console.log("Saving", {age, concerts, cities, years});
+
+    const adapter = new FileSync(path.join(__dirname, '../database/skills.json'));
+    const db = low(adapter);
+
+    db.defaults({ skills: {} }).write(); 
+    db.set('skills', { age, concerts, cities, years }).write();
+
     resolve();
   }
   catch (err) {
